19c7cb2566b4aadb345a47eef887fb6100e5c1c5
tmit00 <mitchell_ty@student.kings.edu.au>
2025-05-23 15:18:16 +1000

    Used datetime to update the date colour depending if it was overdue or not.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, request
 from data import db, Task
 from routes import register_blueprints
+from datetime import datetime
 
 
 
@@ -12,6 +13,13 @@ register_blueprints(app)
 @app.route('/', methods=['GET'])
 def dashboard():
     all_tasks = Task.query.all()
+    today = datetime.today().date()
+
+    for task in all_tasks:
+        task.is_overdue = False
+        if task.date: # Make sure task.date isn't None
+            task_date = task.date if isinstance(task.date, datetime) else datetime.strptime(task.date, "%Y-%m-%d").date()
+            task.is_overdue = task_date < today and not task.is_complete
     return render_template('dashboard.html', tasks=all_tasks)
 
 if __name__ == '__main__':
